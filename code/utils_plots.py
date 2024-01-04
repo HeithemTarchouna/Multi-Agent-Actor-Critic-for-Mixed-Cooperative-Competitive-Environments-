35b332255863a94499ad18e9beb2a543956559dc
Heithem Tarchouna <75651342+HeithemTarchouna@users.noreply.github.com>
2024-01-04 17:24:50 +0100

    updated small issue with plot y_ticks

--- code/utils_plots.py
+++ code/utils_plots.py
@@ -1,6 +1,13 @@
 import os
 from matplotlib import pyplot as plt
 import pandas as pd
+import numpy as np
+
+def calculate_tick_range(values, interval=20):
+    start = np.floor(min(values) / interval) * interval
+    end = np.ceil(max(values) / interval) * interval
+    return np.arange(start, end + 1, interval)
+
 
 # Create a function to save plots
 def save_plot(plt, filename, output_dir):
@@ -14,28 +21,24 @@ def plot_average_episode_rewards(average_rewards, scenario, output_dir):
     plt.ylabel('Average Episode Reward')
     plt.title(f'Average Episode Reward Progress - {scenario}')
     plt.plot(range(1, len(average_rewards) + 1), average_rewards)
+    plt.yticks(calculate_tick_range(average_rewards))
     plt.grid()
     save_plot(plt, f'Average_Episode_Reward_Progress_{scenario}.png', output_dir)
     
-# Define plotting functions without output directory argument
 def plot_average_episode_rewards_rolling(average_rewards, scenario, output_dir):
-    plt.figure(figsize=(12, 6))
+    plt.figure(figsize=(12, 8))
     plt.xlabel('Episode')
     plt.ylabel('Average Episode Reward')
     plt.title(f'Average Episode Reward Progress - {scenario}')
     
-    # Plot the original rewards data
     plt.plot(range(1, len(average_rewards) + 1), average_rewards, alpha=0.3, label='Original')
     
-    # Convert the rewards to a pandas series to use the rolling window functionality
     rewards_series = pd.Series(average_rewards)
-    
-    # Apply a rolling window and take the mean for smoothing
     smoothed_rewards = rewards_series.rolling(window=100).mean()
-    
-    # Plot the smoothed data
+
     plt.plot(range(1, len(average_rewards) + 1), smoothed_rewards, color='red', label='Smoothed (Rolling Mean)')
     
+    plt.yticks(calculate_tick_range(smoothed_rewards.dropna()))
     plt.legend()
     plt.grid()
     save_plot(plt, f'Average_Episode_Reward_Progress_Rolling_Mean{scenario}.png', output_dir)
@@ -46,9 +49,11 @@ def plot_all_agents_rewards(agent_rewards, scenario, output_dir):
     plt.ylabel('Agent Reward')
     plt.title(f'All Agents Reward Progress (agent + avdversary) - {scenario}')
     
+    all_rewards = np.concatenate([rewards for rewards in agent_rewards.values()])
     for agent_name, rewards in agent_rewards.items():
         plt.plot(range(1, len(rewards) + 1), rewards, label=f'Agent {agent_name}')
     
+    plt.yticks(calculate_tick_range(all_rewards))
     plt.legend()
     plt.grid()
     save_plot(plt, f'All_Agents_Reward_Progress_{scenario}.png', output_dir)
@@ -59,11 +64,12 @@ def plot_individual_agent_rewards(epsiode_mean_agent_rewards, agent_name, scenar
     plt.ylabel('Mean Agent Reward')
     plt.title(f'Mean Agent Reward Progress - {scenario} (Agent {agent_name})')
     plt.plot(range(1, len(epsiode_mean_agent_rewards) + 1), epsiode_mean_agent_rewards, label=f'Agent {agent_name}')
+    
+    plt.yticks(calculate_tick_range(epsiode_mean_agent_rewards))
     plt.legend()
     plt.grid()
     save_plot(plt, f'Individual_Agent_Reward_Progress_{scenario}_Agent_{agent_name}.png', output_dir)
-    
-    
+
 def plot_everything(output_dir, scenario, k, score_history_100, score_history, epsiode_mean_agent_rewards):
     # Plot results for different subpolicies
             output_subdir = os.path.join(output_dir, f'scenario_{scenario}', f'k_{k}')
